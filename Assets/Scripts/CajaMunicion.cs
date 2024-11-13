806b8f6684830a80e6685df3b1a23d139fe71470
Andiiie <andreaalsina0@gmail.com>
2024-11-13 21:03:51 +0100

    13.11

---
+++ Assets/Scripts/CajaMunicion.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMunicion : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
