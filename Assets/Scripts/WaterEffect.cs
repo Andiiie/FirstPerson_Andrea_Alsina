8cee3886615e013e165345d2f7478434f96d9baf
Andiiie <andreaalsina0@gmail.com>
2024-11-25 18:20:26 +0100

    25.11.24

---
+++ Assets/Scripts/WaterEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class WaterEffect : MonoBehaviour
+{
+    
+    Volume efecto;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(efecto.profile.TryGet(out LensDistortion distortion))
+        //{
+        //    FloatParameter miVariable = new FloatParameter(Mathf.Cos(velocidad * Time.time));
+        //    FloatParameter miVariable2 = new FloatParameter(Mathf.Sin(velocidad * Time.time));
+        //    distortion.xMultiplier.SetValue(miVariable);
+        //    distortion.yMultiplier.SetValue(miVariable2);
+        //}
+    }
+}
