90cd61ff51ae3f5f4a3d1b8b535c38a63ecb20d8
DESKTOP-GV16N45\Alumno <andreaalsina0@gmail.com>
2025-01-08 14:17:18 +0100

    08.01

--- Assets/Scripts/WaterEffect.cs
+++ Assets/Scripts/WaterEffect.cs
@@ -7,20 +7,15 @@ public class WaterEffect : MonoBehaviour
 {
     
     Volume efecto;
+    [SerializeField] float velocidad;
+
     void Start()
     {
-        
+      efecto = GetComponent<Volume>();    
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if(efecto.profile.TryGet(out LensDistortion distortion))
-        //{
-        //    FloatParameter miVariable = new FloatParameter(Mathf.Cos(velocidad * Time.time));
-        //    FloatParameter miVariable2 = new FloatParameter(Mathf.Sin(velocidad * Time.time));
-        //    distortion.xMultiplier.SetValue(miVariable);
-        //    distortion.yMultiplier.SetValue(miVariable2);
-        //}
+      
     }
 }
